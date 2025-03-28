686a375911a6c86522f659df31e8332cc2f87fa5
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2025-03-28 10:41:47 -0300

    Guia 1 terminada

---
+++ Assets/Scripts/DatosDelUsuario.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DatosDelUsuario : MonoBehaviour
+{
+    public string nombreUsuario;
+    public int edadUsuario;
+    public float alturaUsuario;
+    public bool esDonante;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Mi nombre es " + nombreUsuario + ", tengo " + edadUsuario + ", mido "
+            + alturaUsuario + " y es " + esDonante + " que soy donante de òrganos.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
