5c04a0799138d17b2e00f3b5718e18caafad8339
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2025-04-04 10:48:48 -0300

    Comida de animalitos
    Falta chequear errores

---
+++ Assets/Scripts/ComidaDeAnimalitos.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComidaDeAnimalitos : MonoBehaviour
+{
+    //1. definición de variables
+    public int cantidadDias;
+    public string codigoAnimal;
+    int gramosComidaTotal;
+    int racionGatos = 300;
+    int racionPerrosPeque = 400;
+    int racionPerrosGrandes = 700;
+    int racionAnimalActual;
+    float costoAlimento;
+    float precioPorCienGramos = 80;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //2. Ingreso de datos (más adelante)
+
+        if(codigoAnimal == "G" || codigoAnimal == "g") 
+        {
+            racionAnimalActual = racionGatos;
+        }else if(codigoAnimal == "PP" || codigoAnimal == "pp")
+        {
+            racionAnimalActual = racionPerrosPeque;
+        }else if(codigoAnimal == "PG" || codigoAnimal == "pg")
+        {
+            racionAnimalActual = racionPerrosGrandes;
+        }
+
+
+        //3. Procesamiento de los datos
+
+        gramosComidaTotal = racionAnimalActual * cantidadDias;
+
+        costoAlimento =   gramosComidaTotal / 100 * precioPorCienGramos;
+
+        //4. Mostrar resultado
+
+        Debug.Log("Para ese periodo se necesitan " + gramosComidaTotal + 
+            " gramos de alimento");
+        Debug.Log("Costo alimento: $" + costoAlimento);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
